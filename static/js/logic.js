8909e278041776b907a13dacf52f704caa379eee
knavarre <knavarre67@gmail.com>
2023-03-30 00:08:57 -0700

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -8,6 +8,7 @@ d3.json(queryUrl).then(function (data) {
   createFeatures(data.features);
 });
 
+// Function to create Map Features
 function createFeatures(earthquakeData) {
 
     //Create function to retrieve earthquake magnitude values
@@ -25,34 +26,35 @@ function createFeatures(earthquakeData) {
         else return 'blue'
     };
 
-  // Define a function that we want to run once for each feature in the features array.
-  // Give each feature a popup that describes the place and time of the earthquake.
+  // Create function for Popup information (location, magnitude, depth)
   function onEachFeature(feature, layer) {
     layer.bindPopup(`<h3>${feature.properties.place}</h3>
     <hr><p>magnitude ${feature.properties.mag}
     <hr><p>depth ${feature.geometry.coordinates[2]}m</p>`);
   };
 
+  // Create function for Marker
   function pointToCircle(point, latlng){
     return L.circleMarker(latlng)
   };
 
+  // Create function for Marker style (radius = magnitude, color = depth)
   function earthquakeStyle(geo_feature){
     return {'radius': getMagnitudes(geo_feature), 'color': depthColors(geo_feature)};
   };
 
-  // Create a GeoJSON layer that contains the features array on the earthquakeData object.
-  // Run the onEachFeature function once for each piece of data in the array.
+  // Create a GeoJSON layer that contains the features created through the above functions
   var earthquakes = L.geoJSON(earthquakeData, {
     onEachFeature: onEachFeature, 
     pointToLayer: pointToCircle,
     style: earthquakeStyle
   });
 
-  // Send our earthquakes layer to the createMap function/
+  // Send our earthquakes layer to the createMap function
   createMap(earthquakes);
 }
 
+// Function to create Map 
 function createMap(earthquakes) {
 
   // Create the base layers.
@@ -84,9 +86,7 @@ function createMap(earthquakes) {
     layers: [street, earthquakes]
   });
 
-  // Create a layer control.
-  // Pass it our baseMaps and overlayMaps.
-  // Add the layer control to the map.
+  // Create a layer control.Pass it our baseMaps and overlayMaps. Add to map. 
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(myMap);
